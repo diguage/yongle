b99bf5e69ad5fe4709ab4781b42b4aa6b4d770e7
diguage <leejun119@gmail.com>
2022-07-21 21:12:18 +0800

    增加必要文件

---
+++ src/main/java/com/diguage/yongle/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.diguage.yongle;
+
+/**
+ * @author D瓜哥 · https://www.diguage.com
+ * @since 2022-07-21 20:50:45
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, D瓜哥 · https://www.diguage.com");
+    }
+}
