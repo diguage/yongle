52e23be4631fbcaa1c09296e22cc586ff9e97bfa
diguage <leejun119@gmail.com>
2024-06-18 17:25:10 +0800

    配置Asciidoclet

--- src/main/java/com/diguage/yongle/HelloWorld.java
+++ src/main/java/com/diguage/yongle/HelloWorld.java
@@ -1,6 +1,35 @@
 package com.diguage.yongle;
 
 /**
+ * = AsciiDoclet 示例文档
+ *
+ * 这是示例文档
+ *
+ * == 示例代码
+ *
+ * [source,java]
+ * ----
+ * public class HelloWorld {
+ *     public static void main(String[] args) {
+ *         System.out.println("Hello, D瓜哥 · https://www.diguage.com");
+ *     }
+ * }
+ * ----
+ *
+ * == PlantUML diagram
+ *
+ * [plantuml,test]
+ * ----
+ * class BlockProcessor
+ * class DiagramBlock
+ * class DitaaBlock
+ * class PlantUmlBlock
+ *
+ * BlockProcessor <|-- DiagramBlock
+ * DiagramBlock <|-- DitaaBlock
+ * DiagramBlock <|-- PlantUmlBlock
+ * ----
+ *
  * @author D瓜哥 · https://www.diguage.com
  * @since 2022-07-21 20:50:45
  */
