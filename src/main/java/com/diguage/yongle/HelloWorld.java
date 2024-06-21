d05716d383f00399adf8a4bde0528ffa51d894a3
diguage <leejun119@gmail.com>
2024-06-21 15:38:02 +0800

    优化示例

--- src/main/java/com/diguage/yongle/HelloWorld.java
+++ src/main/java/com/diguage/yongle/HelloWorld.java
@@ -5,10 +5,13 @@ package com.diguage.yongle;
  *
  * 这是示例文档
  *
+ * TIP: 这是一个提示文档！D瓜哥注：经测试，这个图标因为没有相关 CSS，不生效！
+ *
  * == 示例代码
  *
  * [source,java]
  * ----
+ * // @author D瓜哥 · https://www.diguage.com
  * public class HelloWorld {
  *     public static void main(String[] args) {
  *         System.out.println("Hello, D瓜哥 · https://www.diguage.com");
